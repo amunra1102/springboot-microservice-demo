0e800cf4399af88bacd0c3243516608d62c6d843
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-06-14 18:46:17 +0700

    springboot microservice demo

---
+++ user-service/src/main/java/com/hnb/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,14 @@
+package com.hnb.user.VO;
+
+import com.hnb.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+    private User user;
+    private Department department;
+}
